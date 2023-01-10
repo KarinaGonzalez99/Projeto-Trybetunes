aa8cb0c133336ceb26755d88bd2e169f73dbdf9c
Karina Gonzalez <karinagonzalez99@gmail.com>
2023-01-10 14:59:39 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import ProfileEdit from './pages/ProfileEdit';
+import Profile from './pages/Profile';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/notfound" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 // iniciando
